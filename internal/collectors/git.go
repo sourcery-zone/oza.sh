f0ace7c0c23a105b7072736ca714b523084ed8a9
Sourcerer <git+commits@sourcery.zone>
2025-07-12 21:17:26 +0200

    feat(git): add lazy loader constructs.

--- internal/collectors/git.go
+++ internal/collectors/git.go
@@ -3,6 +3,7 @@ package collectors
 import (
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 // isGitDirectory check if `path` is a git repository.
@@ -44,6 +45,43 @@ type GitStatus struct {
 
 type Git struct {
 	path string
+	once sync.Once
+	data GitStatus
+}
 
+// NewGit create a new `Git` object initialized with path.
+func NewGit(path string) *Git {
+	if path == "" {
+		panic("Git: path is required")
+	}
+	return &Git{path: path}
+}
 
+// load the git repository information.
+func (g *Git) load() {
+	g.once.Do(func() {
+		g.data = GitStatus{
+			IsGit: isGitDirectory(g.path),
+			Branch: getGitBranch(g.path),
+			Dirty: isGitDirty(g.path),
+		}
+	})
+}
+
+// IsGit check if the current path is a git repository.
+func (g *Git) IsGit() bool {
+	g.load()
+	return g.data.IsGit
+}
+
+// Branch get current branch name in the repository.
+func (g *Git) Branch() string {
+	g.load()
+	return g.data.Branch
+}
+
+// Dirty get if the repository is dirty (had uncommitted change).
+func (g *Git) Dirty() bool {
+	g.load()
+	return g.data.Dirty
 }
