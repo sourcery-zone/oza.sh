ea0ba288b5b752ed3348d82007cab43967b2a303
Sourcerer <git+commits@sourcery.zone>
2025-07-12 21:07:40 +0200

    fix: indentation of source files.
    This was due to a misconfiguration on `go-ts-mode-indent-offset` of 8
    in my Emacs setup, messing up the number of used indentation tabs by
    2. It's fixed now!

--- internal/collectors/git.go
+++ internal/collectors/git.go
@@ -7,31 +7,43 @@ import (
 
 // isGitDirectory check if `path` is a git repository.
 func isGitDirectory(path string) bool {
-		cmd := exec.Command("git", "-C", path, "rev-parse", "--is-inside-work-tree")
-		err := cmd.Run()
-		return err == nil
+	cmd := exec.Command("git", "-C", path, "rev-parse", "--is-inside-work-tree")
+	err := cmd.Run()
+	return err == nil
 }
 
 // isGitDirty test if the given git repository is dirty (has
 // uncommitted changes).
 func isGitDirty(path string) bool {
-		cmd := exec.Command("git", "-C", path, "status", "--porcelain")
-		output, err := cmd.Output()
-		if err != nil {
-				return false
-		}
+	cmd := exec.Command("git", "-C", path, "status", "--porcelain")
+	output, err := cmd.Output()
+	if err != nil {
+		return false
+	}
 
-		return len(output) > 0
+	return len(output) > 0
 }
 
 // getGitBranch get the current branch name of the git repostiroy.
 func getGitBranch(path string) string {
-		cmd := exec.Command("git", "-C", path, "rev-parse", "--abbrev-ref", "HEAD")
-		output, err := cmd.Output()
-		if err != nil {
-				return ""
-		}
+	cmd := exec.Command("git", "-C", path, "rev-parse", "--abbrev-ref", "HEAD")
+	output, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
 
-		branch := strings.TrimSpace(string(output))
-		return branch
+	branch := strings.TrimSpace(string(output))
+	return branch
+}
+
+type GitStatus struct {
+	IsGit  bool
+	Branch string
+	Dirty  bool
+}
+
+type Git struct {
+	path string
+
+
 }
